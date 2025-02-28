6d686398a6f921f1bfdeb6991bf5c8b96788bac9
CHANDRASHEKHAR BADGUJAR <cbadgujar2050@gmail.com>
2025-02-28 11:42:03 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,15 @@
+package BASIC;
+
+public class First {
+	public static void main(String Chandrashekhar_Badgujar_Java_args[]) {
+	//public static void main(String args[]) {
+		String nm="Chandrashekhar";
+		int age=31;
+		float per=81.23f;
+		System.out.println("Welcome to java");
+		System.out.println("Name="+nm);
+		System.out.println("Age="+age);
+		System.out.println("percentage="+per+"%");
+	}
+
+}
